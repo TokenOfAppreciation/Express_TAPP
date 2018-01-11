3d8979542815c9a7fdc7e354749096f8f9a5b2ff
rappai13 <rappai13@users.noreply.github.com>
2018-01-11 15:59:14 +0100

    Initialized main.js and set web3provider

---
+++ public/main.js
@@ -0,0 +1,14 @@
+var web3 = require('web3');
+
+var TAPP = {
+
+  
+window.addEventListener('load', function() {
+  if (typeof web3 !== 'undefined') {
+    console.log("Using external provider.");
+    window.web3 = new Web3(web3.currentProvider);
+  } else {
+    console.log("Using default provider"); //mostly for testing&debugging
+    window.web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:9545"));
+  }
+}
