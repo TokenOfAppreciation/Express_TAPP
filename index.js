3d29f1629a70266115a7b63e5eb949e9b31879e1
Kl4rity <clemens@stift.online>
2018-01-11 14:51:24 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const http = require('http');
+const logger = require('morgan');
+const path = require('path');
+
+// const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(logger('short'));
+
+let publicPath = path.resolve(__dirname, "public");
+app.use(express.static(publicPath));
+
+// -- configuration
+let port = process.env.PORT || 3000;
+
+http.createServer(app).listen(port);
