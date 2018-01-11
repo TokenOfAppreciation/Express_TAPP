7edfcc2d784d9a275abacb85ca3563cd710b8247
rappai13 <rappai13@users.noreply.github.com>
2018-01-11 16:30:40 +0100

    Added web3.min.js

--- public/main.js
+++ public/main.js
@@ -4,12 +4,12 @@ const TAPP = {
 
 
 window.addEventListener('load', function() {
-  if (typeof web3 !== 'undefined') {
-    console.log("Using external provider.");
+  if (typeof web3 !== 'undefined' && web3.version.network === 4) {
+    console.log("Using external provider on Rinkeby.");
     window.web3 = new Web3(web3.currentProvider);
   } else {
-    console.log("Using default provider"); //mostly for testing&debugging
-    window.web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:9545"));
+    console.warn("TAP is only accessible via the Rinkeby-Testnet"); //mostly for testing&debugging
+    //window.web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:9545"));
   }
 }
 
