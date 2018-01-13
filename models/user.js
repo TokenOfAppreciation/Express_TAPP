fbbabf52ff446839610bf8ed33c8434596f2af73
Kl4rity <clemens@stift.online>
2018-01-13 20:48:03 +0100

    Added MongoDB User Schema

---
+++ models/user.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt-nodejs');
+const SALT_FACTOR = 10;
+
+const userSchema = mongoose.Schema({
+  username: {type: String, required: true, unique: true},
+  // facebookThirdPartyID: {type: String, required: true},
+  password: {type: String, required: true},
+  createdAt: {type: Date, default: Date.now}
+});
+
+userSchema.methods.name = function() {
+  return this.username;
+};
+
+let noop = function(){};
+
+userSchema.pre("save", function(done){
+  var user = this;
+  if (!user.isModified("password")){
+    return done();
+  }
+  bcrypt.genSalt(SALT_FACTOR, function(err, salt){
+    if (err) {return done(err);}
+    bcrypt.hash(user.password, salt, noop,
+      function(err, hashedPassword){
+        if (err){return done(err);}
+        user.password = hashedPassword;
+        done();
+      });
+  });
+});
+
+userSchema.methods.checkPassword = function(guess, done){
+  bcrypt.compare(guess, this.password, function(err, isMatch){
+    done(err, isMatch);
+  });
+};
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
