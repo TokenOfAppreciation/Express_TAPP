c0d0f705903c6c7e93b41aba32746b498622510c
Kl4rity <clemens@stift.online>
2018-01-25 16:25:47 +0100

    Deleted unused/commented out code.

--- models/user.js
+++ models/user.js
@@ -1,16 +1,10 @@
 const mongoose = require('mongoose');
 const findOrCreate = require('mongoose-findorcreate');
-// const bcrypt = require('bcrypt-nodejs');
-// const SALT_FACTOR = 10;
 
 const userSchema = mongoose.Schema({
   email: {type: String, required: true},
   assigned: {type: Boolean},
   address: {type: String}
-  // username: {type: String, required: true},
-  // facebookID: {type: String, required: true},
-  // facebookThirdPartyID: {type: String, required: true, default: "N/A"},
-  // createdAt: {type: Date, default: Date.now}
 });
 
 userSchema.methods.name = function() {
@@ -18,30 +12,6 @@ userSchema.methods.name = function() {
 };
 
 userSchema.plugin(findOrCreate);
-
-// let noop = function(){};
-
-// userSchema.pre("save", function(done){
-//   var user = this;
-//   if (!user.isModified("password")){
-//     return done();
-//   }
-//   bcrypt.genSalt(SALT_FACTOR, function(err, salt){
-//     if (err) {return done(err);}
-//     bcrypt.hash(user.password, salt, noop,
-//       function(err, hashedPassword){
-//         if (err){return done(err);}
-//         user.password = hashedPassword;
-//         done();
-//       });
-//   });
-// });
-
-// userSchema.methods.checkPassword = function(guess, done){
-//   bcrypt.compare(guess, this.password, function(err, isMatch){
-//     done(err, isMatch);
-//   });
-// };
 
 const User = mongoose.model("User", userSchema);
 module.exports = User;
